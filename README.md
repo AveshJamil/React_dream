7dfc6966dd3ad46c731f3675216738b9e478aa25
Avesh <avesh.jam721@gmail.com>
2024-02-23 22:57:31 +0530

    my changese

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # hello React 
+
+# parsel 
+- Dev Build ;
+- Local server ;
+- HMR ->Hot module Replacement ;
+- File Watching Algorithm Written in c++;
+
  
--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 
 const heading=React.createElement('div',{id:'parent'},
 React.createElement('div',{id:'child'},[
@@ -16,9 +19,10 @@ React.createElement('div',{id:'child2'},[
 
 const root=ReactDOM.createRoot(document.getElementById('root'));
 
-//console.log(heading);
 
-root.render(heading);
+root.render(heading);  
+
+
 
 
 
